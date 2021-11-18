73fa635621c25cea7a58a5a54d81c87f835fab08
Daniel Puente <dpr1005@alu.ubu.es>
2021-11-18 21:12:09 +0100

    ENN removing noisy samples #4

--- Instance Selection Algorithms/ENN.py
+++ Instance Selection Algorithms/ENN.py
@@ -3,7 +3,7 @@
 # @Filename:    ENN.py
 # @Author:      Daniel Puente Ramírez
 # @Time:        16/11/21 17:14
-import copy
+
 import numpy as np
 from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
@@ -21,7 +21,6 @@ def ENN(X, k):
     :return:
     """
     y = X.target
-    S1 = copy.deepcopy(X)
     borrado = 0
     for index, flower in enumerate(X.data):
         classes = {}
@@ -35,12 +34,11 @@ def ENN(X, k):
                 classes[neigh] = 0
         if max(classes, key=classes.get) != y[index - 1]:
             borrado += 1
-            S1['target'][index - 1] = 3
             np.delete(X.data, index - 1, 0)
             np.delete(X.target, index - 1, 0)
 
     print(f"Se han eliminado {borrado} muestras.")
-    return X, S1
+    return X
 
 
 def main():
@@ -50,17 +48,17 @@ def main():
     target_names.append('deleted')
     data['target_names'] = np.array(target_names)
 
-    S, S1 = ENN(X=data, k=k)
+    S = ENN(X=data, k=k)
 
     # https://scipy-lectures.org/packages/scikit-learn/auto_examples/plot_iris_scatter.html
     x_index = 0
     y_index = 1
-    formatter = plt.FuncFormatter(lambda i, *args: S1.target_names[int(i)])
+    formatter = plt.FuncFormatter(lambda i, *args: S.target_names[int(i)])
     plt.figure(figsize=(5, 4))
-    plt.scatter(S1.data[:, x_index], S1.data[:, y_index], c=S1.target)
+    plt.scatter(S.data[:, x_index], S.data[:, y_index], c=S.target)
     plt.colorbar(ticks=[0, 1, 2, 3], format=formatter)
-    plt.xlabel(S1.feature_names[x_index])
-    plt.ylabel(S1.feature_names[y_index])
+    plt.xlabel(S.feature_names[x_index])
+    plt.ylabel(S.feature_names[y_index])
 
     plt.tight_layout()
     plt.show()
