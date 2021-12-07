eafe5bcf4dcb19f7f9a5b1b1813a845448e75520
Daniel Puente <dpr1005@alu.ubu.es>
2021-12-07 12:33:50 +0100

    Added support for testing multiple precision values  #17

--- Instance Selection Algorithms/testing_unlabeled.py
+++ Instance Selection Algorithms/testing_unlabeled.py
@@ -4,15 +4,17 @@
 # @Author:      Daniel Puente Ramírez
 # @Time:        6/12/21 09:44
 
-import csv
 import copy
+import csv
 import os.path
+from math import floor
 from os import walk
 
-from sklearn.model_selection import train_test_split
-from sklearn.utils import Bunch
-from sklearn.tree import DecisionTreeClassifier
+import numpy as np
 from sklearn.metrics import accuracy_score, mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.utils import Bunch
 
 from CNN import CNN
 from ENN import ENN
@@ -23,34 +25,37 @@ from testing import arff2sk_dataset
 
 
 def main():
-    precision = 0.01
+    precisions = [0.01, 0.05, 0.1, 0.2, 0.5]
     datasets = next(walk('../datasets'), (None, None, []))[2]
     datasets.sort()
     header = ['dataset', 'ENN', 'CNN', 'RNN', 'ICF', 'MSS']
-    csv_path_acc = f'testing_out/test_unlabeled_{precision}_acc.csv'
-    csv_path_mse = f'testing_out/test_unlabeled_{precision}_mse.csv'
-    with open(csv_path_acc, 'w') as save:
-        w = csv.writer(save)
-        w.writerow(header)
-        save.close()
-    with open(csv_path_mse, 'w') as save:
-        w = csv.writer(save)
-        w.writerow(header)
-        save.close()
-    acc = []
-    mse = []
-    random_state = 0x1122021
+    for precision in precisions:
+        csv_path_acc = f'testing_out/test_unlabeled_{precision}_acc.csv'
+        csv_path_mse = f'testing_out/test_unlabeled_{precision}_mse.csv'
+        with open(csv_path_acc, 'w') as save:
+            w = csv.writer(save)
+            w.writerow(header)
+            save.close()
+        with open(csv_path_mse, 'w') as save:
+            w = csv.writer(save)
+            w.writerow(header)
+            save.close()
+        acc = []
+        mse = []
+        random_state = 0x1122021
 
-    for path in datasets[:10]:
-        name = path.split('.')[0]
-        print(f'Starting {name} dataset...')
-        d1 = arff2sk_dataset(os.path.join('../datasets/', path))
-        print(f'\t{len(d1["data"])} samples.')
-        current_dataset = [name]
-        results_dataset = __evaluate__(dataset=d1, precision=precision,
-                                       random_state=random_state)
-        acc.append(current_dataset + results_dataset[:, 0])
-        mse.append(current_dataset + results_dataset[:, 1])
+        for path in datasets[:10]:
+            name = path.split('.')[0]
+            print(f'Starting {name} dataset...')
+            d1 = arff2sk_dataset(os.path.join('../datasets/', path))
+            print(f'\t{floor(len(d1["data"]) * precision)} samples.')
+            current_dataset = [name]
+            results_dataset = np.array(__evaluate__(dataset=d1,
+                                                    precision=precision,
+                                                    random_state=random_state))
+
+            acc.append(current_dataset + list(results_dataset[:, 0]))
+            mse.append(current_dataset + list(results_dataset[:, 1]))
 
         with open(csv_path_acc, 'a') as save:
             w = csv.writer(save)
@@ -66,6 +71,7 @@ def __evaluate__(dataset, precision, random_state):
     algorithms = [ENN, CNN, RNN, ICF, MSS]
     current_dataset = []
     for algorithm in algorithms:
+        print(f"\n{algorithm.__name__}________")
         data = copy.copy(dataset)
         X = data['data']
         y = data['target']
@@ -77,12 +83,18 @@ def __evaluate__(dataset, precision, random_state):
             data_alg = algorithm(X=Bunch(data=X_train, target=y_train))
         else:
             data_alg = algorithm(X=Bunch(data=X_train, target=y_train), k=3)
-        acc, mse = __train_and_predict__(data_alg,
-                                         Bunch(data=X_test, target=y_test))
-        print(f"\t\tacc: {acc:.3f}")
-        print(f"\t\tmse: {mse:.3f}")
+
+        if len(data_alg['data']) != 0:
+            acc, mse = __train_and_predict__(data_alg,
+                                             Bunch(data=X_test, target=y_test))
+            print(f"\t|-> acc: {acc:.3f}\n\t|-> mse: {mse:.3f}")
+        else:
+            acc = '-'
+            mse = '-'
+            print(f"\t|-> acc: {acc}\n\t|-> mse: {mse}")
+
         current_dataset.append([acc, mse])
-        return current_dataset
+    return current_dataset
 
 
 def __train_and_predict__(data_alg, data_test):
@@ -92,6 +104,7 @@ def __train_and_predict__(data_alg, data_test):
     acc = accuracy_score(prediction, data_test['target'])
     mse = mean_squared_error(prediction, data_test['target'])
     return acc, mse
+
 
 if __name__ == '__main__':
     main()
