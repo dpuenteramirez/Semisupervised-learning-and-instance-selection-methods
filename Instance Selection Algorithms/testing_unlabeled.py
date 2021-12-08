ca00e65b78976ada873a2f048bfb82fd34717fd5
Daniel Puente <dpr1005@alu.ubu.es>
2021-12-08 10:01:23 +0100

    Added cross-validation support #17

--- Instance Selection Algorithms/testing_unlabeled.py
+++ Instance Selection Algorithms/testing_unlabeled.py
@@ -9,10 +9,11 @@ import csv
 import os.path
 from math import floor
 from os import walk
+from statistics import mean
 
 import numpy as np
 from sklearn.metrics import accuracy_score, mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.utils import Bunch
 
@@ -30,10 +31,12 @@ def main():
     datasets.sort()
     header = ['dataset', 'ENN', 'CNN', 'RNN', 'ICF', 'MSS']
     for precision in precisions:
-        print(f"\n\nCurrent precision: {precision*100:>2}%")
+        print(f"\n\nCurrent precision: {precision * 100:>2}%")
         print("-----------------------")
-        csv_path_acc = f'testing_out/test_unlabeled_{precision}_acc.csv'
-        csv_path_mse = f'testing_out/test_unlabeled_{precision}_mse.csv'
+        csv_path_acc = f'testing_out/test_unlabeled_cross_validation' \
+                       f'_{precision}_acc.csv'
+        csv_path_mse = f'testing_out/test_unlabeled_cross_validation' \
+                       f'_{precision}_mse.csv'
         with open(csv_path_acc, 'w') as save:
             w = csv.writer(save)
             w.writerow(header)
@@ -75,27 +78,41 @@ def __evaluate__(dataset, precision, random_state):
     for algorithm in algorithms:
         print(f"\n{algorithm.__name__}________")
         data = copy.copy(dataset)
+        kf = KFold(n_splits=floor(len(data['data']) * precision), shuffle=True,
+                   random_state=random_state)
         X = data['data']
         y = data['target']
-        X_train, X_test, y_train, y_test = \
-            train_test_split(X, y, train_size=precision,
-                             random_state=random_state, stratify=y)
+        acc = []
+        mse = []
+        exit = False
+        for train_index, test_index in kf.split(X):
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
 
-        if algorithm.__name__ != "ENN":
-            data_alg = algorithm(X=Bunch(data=X_train, target=y_train))
+            if algorithm.__name__ != "ENN":
+                data_alg = algorithm(X=Bunch(data=X_train, target=y_train))
+            else:
+                data_alg = algorithm(X=Bunch(data=X_train, target=y_train), k=3)
+
+            if len(data_alg['data']) != 0:
+                acc_t, mse_t = __train_and_predict__(data_alg,
+                                                     Bunch(data=X_test,
+                                                           target=y_test))
+                acc.append(acc_t)
+                mse.append(mse_t)
+            else:
+                acc = '-'
+                mse = '-'
+                print(f"\t|-> acc: {acc}\n\t|-> mse: {mse}")
+                exit = True
+                break
+
+        print(f"\t|-> acc: {mean(acc):.3f}\n\t|-> mse: {mean(mse):.3f}")
+
+        if not exit:
+            current_dataset.append([acc, mse])
         else:
-            data_alg = algorithm(X=Bunch(data=X_train, target=y_train), k=3)
-
-        if len(data_alg['data']) != 0:
-            acc, mse = __train_and_predict__(data_alg,
-                                             Bunch(data=X_test, target=y_test))
-            print(f"\t|-> acc: {acc:.3f}\n\t|-> mse: {mse:.3f}")
-        else:
-            acc = '-'
-            mse = '-'
-            print(f"\t|-> acc: {acc}\n\t|-> mse: {mse}")
-
-        current_dataset.append([acc, mse])
+            current_dataset.append(['-', '-'])
     return current_dataset
 
 
