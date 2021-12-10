4f0d2bb23c7271ef710e7616dbefafab9d0bdc0a
Daniel Puente <dpr1005@alu.ubu.es>
2021-12-10 11:56:20 +0100

    Added first approach to self-training #22

---
+++ Instance Selection Algorithms/self_training.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    self_training.py
+# @Author:      Daniel Puente Ramírez
+# @Time:        10/12/21 08:36
+from os import walk
+import os.path
+from testing import arff2sk_dataset
+from math import floor
+import copy
+import csv
+import numpy as np
+from statistics import mean
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.semi_supervised import SelfTrainingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def main():
+    precisions = [0.1, 0.2, 0.5]  # [0.01, 0.05, 0.1, 0.2, 0.5]
+    datasets = next(walk('../datasets'), (None, None, []))[2]
+    datasets.sort()
+    random_state = 0x10122021
+
+    csv_path = f'test_self_training/accuracy_self_training.csv'
+    header = ['dataset'] + precisions
+
+    with open(csv_path, 'w') as save:
+        w = csv.writer(save)
+        w.writerow(header)
+        save.close()
+
+    for path in datasets[:10]:
+        name = path.split('.')[0]
+        print(f'Starting {name} dataset...')
+        d1 = arff2sk_dataset(os.path.join('../datasets/', path))
+
+        current_dataset = [name]
+
+        for precision in precisions:
+            print(f"\n\nCurrent precision: {precision * 100:>2}%")
+            data = copy.copy(d1)
+            print(f'\t{floor(len(data["data"]) * precision)} samples.\t',
+                  end='\n')
+
+            X = data['data']
+            y = data['target']
+
+            acc_temp = []
+            kf = KFold(n_splits=10, shuffle=True, random_state=random_state)
+            for train_index, test_index in kf.split(X):
+                y[train_index] = -1
+                X_train, X_test = X[train_index], X[test_index]
+                y_train, y_test = y[train_index], y[test_index]
+
+                X_train, _, y_train, _ = train_test_split(X_train, y_train,
+                                                          test_size=
+                                                          1 - precision,
+                                                          random_state=
+                                                          random_state)
+
+                neigh = KNeighborsClassifier(n_neighbors=3)
+                classifier = SelfTrainingClassifier(neigh)
+                classifier.fit(X_train, y_train)
+                exit(0)
+                prediction = classifier.predict(X_test)
+                acc_temp.append(accuracy_score(prediction, y_test))
+                print(f'acc: {mean(acc_temp):.3f}')
+
+            current_dataset.append(mean(acc_temp))
+            with open(csv_path, 'a') as save:
+                w = csv.writer(save)
+                w.writerow(current_dataset)
+                save.close()
+
+
+if __name__ == '__main__':
+    main()
