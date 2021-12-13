f3768127962a8caa205618d49963dfa269babd93
Daniel Puente <dpr1005@alu.ubu.es>
2021-12-13 10:21:45 +0100

    Added code to run the tests based on percent of self-training #22

--- Instance Selection Algorithms/self_training_test_percent.py
+++ Instance Selection Algorithms/self_training_test_percent.py
@@ -18,7 +18,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 def main():
-    precisions = [0.1, 0.2, 0.5]  # [0.01, 0.05, 0.1, 0.2, 0.5]
+    precisions = [0.01, 0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 1.0]
     datasets = next(walk('../datasets'), (None, None, []))[2]
     datasets.sort()
     random_state = 0x10122021
@@ -50,29 +50,33 @@ def main():
             acc_temp = []
             kf = KFold(n_splits=10, shuffle=True, random_state=random_state)
             for train_index, test_index in kf.split(X):
-                y[train_index] = -1
                 X_train, X_test = X[train_index], X[test_index]
                 y_train, y_test = y[train_index], y[test_index]
 
-                X_train, _, y_train, _ = train_test_split(X_train, y_train,
-                                                          test_size=
-                                                          1 - precision,
-                                                          random_state=
-                                                          random_state)
+                if precision != 1.0:
+                    indexes = np.random.choice(len(y_train),
+                                               floor(len(y_train) * (
+                                                           1 - precision)),
+                                               replace=False)
+                    indexes = np.setdiff1d(np.arange(len(y_train)), indexes)
+                    y_train[indexes] = -1
+                    X_train, _, y_train, _ = \
+                        train_test_split(X_train, y_train,
+                                         test_size=1 - precision,
+                                         random_state=random_state)
 
                 neigh = KNeighborsClassifier(n_neighbors=3)
-                classifier = SelfTrainingClassifier(neigh)
+                classifier = SelfTrainingClassifier(neigh, verbose=True)
                 classifier.fit(X_train, y_train)
-                exit(0)
                 prediction = classifier.predict(X_test)
                 acc_temp.append(accuracy_score(prediction, y_test))
                 print(f'acc: {mean(acc_temp):.3f}')
 
             current_dataset.append(mean(acc_temp))
-            with open(csv_path, 'a') as save:
-                w = csv.writer(save)
-                w.writerow(current_dataset)
-                save.close()
+        with open(csv_path, 'a') as save:
+            w = csv.writer(save)
+            w.writerow(current_dataset)
+            save.close()
 
 
 if __name__ == '__main__':
