f364232a7673e0a3ad154d943b97847910ef5797
Daniel Puente <dpr1005@alu.ubu.es>
2021-12-25 13:09:55 +0100

    Implemented Co-Training #36

--- SemiSupervised Learning Algorithms/Co-Training.py
+++ SemiSupervised Learning Algorithms/Co-Training.py
@@ -4,19 +4,21 @@
 # @Author:      Daniel Puente Ramírez
 # @Time:        22/12/21 09:27
 
+import argparse
 import sys
 from math import floor
-import pandas as pd
-import numpy as np
+from os import walk
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 from numpy.random import randint
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.utils import Bunch
 from tqdm import trange
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
 sys.path.insert(0, "../utils")
 from arff2dataset import arff2sk_dataset
@@ -24,14 +26,21 @@ from arff2dataset import arff2sk_dataset
 
 def co_training(p, n, k, u, L, U):
     tot = p + n
+    counts = {}
+    for tar in L['class_names']:
+        counts[tar] = 0
+    for tar in L['target']:
+        counts[tar] += 1
+
+    p_class = max(counts, key=counts.get)
 
     rng = np.random.default_rng()
-
-    model = RandomForestClassifier(n_estimators=100, n_jobs=-1)
+    model = RandomForestClassifier(n_jobs=-1)
 
     __U__ = rng.choice(U['data'], size=u, replace=False, shuffle=False)
 
     for _ in trange(k):
+
         size = floor(len(L['data']) / 2)
         rem = len(L['data']) - size
         x1, y1 = L['data'][:size], L['target'][:size]
@@ -43,6 +52,7 @@ def co_training(p, n, k, u, L, U):
         pred_2, pred_proba_2 = h2.predict(__U__), h2.predict_proba(__U__)
 
         idx_1 = np.amax(pred_proba_1, axis=1)
+        aa = pd.DataFrame(pred_proba_1)
         idx_2 = np.amax(pred_proba_2, axis=1)
         prediction_1 = [[x, y, z, u] for x, y, z, u in zip(pred_1, pred_proba_1,
                                                            idx_1, __U__)]
@@ -52,12 +62,27 @@ def co_training(p, n, k, u, L, U):
         prediction_1.sort(key=lambda x: x[2], reverse=True)
         prediction_2.sort(key=lambda x: x[2], reverse=True)
 
-        predictions = np.concatenate((prediction_1[:tot], prediction_2[
-                                                          :tot]), axis=0)
+        predictions = np.concatenate((prediction_1, prediction_2), axis=0)
+
+        predictions_temp = pd.DataFrame(predictions, columns=['Class', 'Array',
+                                                              'Confidence',
+                                                              'Sample'])
+        predictions = []
+
+        p_added = 0
+        for _, pred in predictions_temp.iterrows():
+            if len(predictions) == tot:
+                break
+            if pred['Class'] == p_class and p_added < p:
+                predictions.append(pred)
+                p_added += 1
+            elif pred['Class'] != p_class:
+                predictions.append(pred)
 
         predictions = pd.DataFrame(predictions, columns=['Class', 'Array',
                                                          'Confidence',
                                                          'Sample'])
+
         samples = np.array([x for x in predictions.pop('Sample').to_numpy()],
                            dtype=object)
         classes = np.array([x for x in predictions.pop('Class').to_numpy()])
@@ -72,22 +97,36 @@ def co_training(p, n, k, u, L, U):
 
 
 if __name__ == '__main__':
-    # dataset = load_iris()
-    #dataset = arff2sk_dataset('../datasets/08_segment_norm.arff')
-    #dataset = arff2sk_dataset('../datasets/03_contraceptive_norm.arff')
-    dataset = arff2sk_dataset('../datasets/25_coil2000_norm.arff')
-    # dataset = arff2sk_dataset('../datasets/16_page-blocks_norm.arff')
+    folder = '../datasets/'
 
-    target_labels = [int(x) for x in dataset.class_names]
+    files = next(walk(folder), (None, None, []))[2]
+    files_tot = '\t'.join(files)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--d', nargs=1)
+    args = parser.parse_args()
 
+    if args.d[0] not in files_tot:
+        print('File does not exist.')
+        exit(1)
+    dataset = [file for file in files if args.d[0] in file]
+    if len(dataset) != 1:
+        print('More than one file found.')
+        exit(2)
+
+    dataset = arff2sk_dataset(folder + dataset[0])
+
+    target_labels = [int(float(x)) for x in dataset.class_names]
     y = dataset.target
     X = dataset.data
-    X_test, X_train, y_test, y_train = train_test_split(X, y, train_size=0.75)
-    X = Bunch(data=X_train, target=y_train)
+    X_test, X_train, y_test, y_train = train_test_split(X, y,
+                                                        train_size=0.95,
+                                                        stratify=y)
+    X = Bunch(data=X_train, target=y_train, class_names=target_labels)
     y = Bunch(data=X_test)
 
-    data, target = co_training(p=1, n=3, k=20, u=10, L=X, U=y)
-    model = KNeighborsClassifier(n_jobs=-1, n_neighbors=1)
+    data, target = co_training(p=3, n=9, k=30, u=20, L=X, U=y)
+
+    model = RandomForestClassifier(n_jobs=-1)
     model.fit(data, target)
     y_pred = model.predict(X_test)
 
