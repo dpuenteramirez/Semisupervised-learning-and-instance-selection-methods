71ae859cb5c4e0847973fa25609dd54dc2e5f305
Daniel Puente <dpr1005@alu.ubu.es>
2022-02-09 20:12:38 +0100

    Modded files to allow global stats #107

--- hypothesis/rankings.py
+++ hypothesis/rankings.py
@@ -4,12 +4,12 @@
 # @Author:      Daniel Puente Ramírez
 # @Time:        6/2/22 17:43
 
-import os
 import numpy as np
 import pandas as pd
 from os import walk
 from os.path import join
-
+from utils.dir import check_dir
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     folder = join('.', 'tests', 'ranks')
@@ -18,30 +18,60 @@ if __name__ == '__main__':
     results_found = next(walk(folder), (None, None, []))[2]
     results_found.sort()
 
-    if not os.path.isdir(join(folder, ranked)):
-        os.mkdir(join(folder, ranked))
-        if os.path.isdir(join(folder, ranked)):
-            print(f'Created main folder for ranks solutions.'
-                  f' {join(folder, ranked)}')
-        else:
-            print(f'Create manually the folder \'{ranked}\' inside {folder} and'
-                  f' rerun.')
+    check_dir(folder)
+    check_dir(join(folder, ranked))
 
     working_results = [r for r in results_found if training_method in r]
-    if len(working_results) > 1:
+    if len(working_results) > 9:
         print('Select the date to visualize:')
         for i, result in enumerate(working_results):
             print(f'{i}.', result.split('.')[0].split('_')[-1])
-        date = int(input(f'Select [0-{len(working_results)}]'))
+        date = int(input(f'Select [0-{len(working_results) - 1}]'))
     else:
-        date = 0
+        date = 3
+
+    results_dir = join(folder, ranked, working_results[date].split('.')[0])
+    check_dir(results_dir)
 
     df = pd.read_csv(join(folder, working_results[date]))
     df = df.fillna(0)
-    df_1 = df.groupby(['dataset', '% labeled'])
-    df_1 = df[['dataset', '% labeled']].copy().join(df_1.rank(axis=1))
-    df_1.to_csv(join(folder, ranked, working_results[date]), index=False)
-    print(f'Results in {join(folder, ranked, working_results[date])}')
+    df_fin = pd.DataFrame(columns=df.keys())
 
+    for row in df.iterrows():
+        arr = row[1][2:]
+        if len(np.unique(arr)) + 2 == len(arr):
+            ranks = [1, 1, 1]
+        elif len(np.unique(arr)) + 1 == len(arr):
+            idx = np.where(arr == arr[0])
+            if len(idx) == 1 and arr[0] > arr[1] and arr[0] > arr[2]:
+                ranks = [1, 2, 2]
+            else:
+                if arr[0] == arr[2] and arr[1] > arr[0]:
+                    ranks = [2, 1, 2]
+                else:
+                    ranks = [2, 2, 1]
+        else:
+            order = arr.argsort()
+            ranks = [x + 1 for x in order.argsort().tolist()]
+        df_fin = df_fin.append(pd.DataFrame([[*row[1][:2], *ranks]],
+                                            columns=df.keys()),
+                               ignore_index=True)
 
+    df_fin.to_csv(join(results_dir, 'ranks.csv'), index=False)
 
+    df_means = df[df.keys()[1:]]
+    df_means = df_means.groupby('% labeled').mean()
+    df_means.to_csv(join(results_dir, 'summary.csv'))
+    df_means.plot(
+        title="Summary of experiments",
+        ylabel="Precision",
+        grid=True,
+        xticks=df_fin['% labeled']
+    )
+    plt.savefig(join(results_dir, 'summary.png'))
+
+    # df_1 = df.groupby(['dataset', '% labeled'])
+    # df_1 = df[['dataset', '% labeled']].copy().join(df_1.rank(axis=1,
+    #                                                           method='dense'))
+    # df_1.to_csv(join(folder, ranked, working_results[date]), index=False)
+    print(f'Results in {results_dir}')
---
+++ utils/dir.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    dir.py
+# @Author:      Daniel Puente Ramírez
+# @Time:        9/2/22 19:15
+
+import os
+
+
+def check_dir(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+        if os.path.isdir(path):
+            print(f'Created main folder for ranks solutions.'
+                  f' {path}')
+        else:
+            print(f'Create manually the folder \'{path.split("/")[-1]}\' inside'
+                  f' {path.split("/")[:-1]} and  rerun.')
+            exit(1)
+    else:
+        print(f'Path OK - {path}')
