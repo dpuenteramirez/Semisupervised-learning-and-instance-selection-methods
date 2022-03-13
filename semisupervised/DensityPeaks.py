0f12600cb08542f94c594b21a39ae6964a5f32d7
Daniel Puente <dpr1005@alu.ubu.es>
2022-03-13 17:23:13 +0100

    Added STDPNF #12 #119

--- semisupervised/DensityPeaks.py
+++ semisupervised/DensityPeaks.py
@@ -3,13 +3,16 @@
 # @Filename:    DensityPeaks.py
 # @Author:      Daniel Puente Ramírez
 # @Time:        5/3/22 09:55
-# @Version:     2.0
+# @Version:     3.0
 
 import math
+from collections import defaultdict
 
 import numpy as np
 import pandas as pd
+from sklearn.neighbors import NearestNeighbors, KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.semi_supervised import SelfTrainingClassifier
 from sklearn.svm import SVC
 
 
@@ -18,16 +21,18 @@ class SemiSupervisedSelfTraining:
     def __init__(self,
                  dc=None,
                  distance_metric='euclidean',
+                 k=3,
                  gauss_cutoff=True,
                  density_threshold=None,
                  distance_threshold=None,
                  anormal=True,
                  filtering=False,
-                 classifier=None
+                 classifier=None,
                  ):
         """Semi Supervised Algorithm based on Density Peaks."""
         self.dc = dc
         self.distance_metric = distance_metric
+        self.k = k
         self.gauss_cutoff = gauss_cutoff
         self.density_threshold = density_threshold
         self.distance_threshold = distance_threshold
@@ -35,7 +40,7 @@ class SemiSupervisedSelfTraining:
         self.filtering = filtering
         self.classifier = classifier
 
-    def build_distance(self):
+    def __build_distance(self):
         """
         Calculate distance dict.
 
@@ -59,7 +64,7 @@ class SemiSupervisedSelfTraining:
 
         return distance, max_dis, min_dis
 
-    def auto_select_dc(self):
+    def __auto_select_dc(self):
         """
         Auto select the local density threshold that let average neighbor is 1-2 percent of all nodes.
 
@@ -83,14 +88,14 @@ class SemiSupervisedSelfTraining:
                 break
         return dc
 
-    def select_dc(self):
+    def __select_dc(self):
         """
         Select the local density threshold, default is the method used in paper, 'auto' is auto select.
 
         :return: dc that local density threshold
         """
         if self.dc == 'auto':
-            dc = self.auto_select_dc()
+            dc = self.__auto_select_dc()
         else:
             percent = 2.0
             position = int(self.n_id * (self.n_id + 1) / 2 * percent / 100)
@@ -99,7 +104,7 @@ class SemiSupervisedSelfTraining:
 
         return dc
 
-    def local_density(self):
+    def __local_density(self):
         """
         Compute all points' local density.
 
@@ -116,7 +121,7 @@ class SemiSupervisedSelfTraining:
                 rho[j] += temp
         return np.array(rho, np.float32)
 
-    def min_neighbor_and_distance(self):
+    def __min_neighbor_and_distance(self):
         """
         Compute all points' min util to the higher local density point(which is the nearest neighbor).
 
@@ -154,25 +159,98 @@ class SemiSupervisedSelfTraining:
                                                              'label']) \
             .transpose()
 
+        self.structure_stdnpf = self.structure.copy(deep=True)
+
     def __step_a(self):
         samples_labeled = self.structure.loc[self.structure['label'] != -1]
         l = samples_labeled['sample'].to_list()
-        self.y = samples_labeled['label'].to_list()
-        self.classifier.fit(l, self.y)
+        y_without = samples_labeled['label'].to_list()
+        self.classifier.fit(l, y_without)
         return samples_labeled
 
-    def _fit_without(self, l, u):
+    def __discover_structure(self):
+        self._fit_without()
+
+    def __nan_search(self):
+        r = 1
+        nan = defaultdict(set)
+        nb = dict.fromkeys(range(self.n_id), 0)
+        knn = defaultdict(set)
+        rnn = defaultdict(set)
+        cnt = defaultdict(int)
+
+        while True:
+            search = NearestNeighbors(n_neighbors=r + 1, algorithm='kd_tree')
+            search.fit(self.data)
+            for index, sample in enumerate(self.data):
+                r_neighs = search.kneighbors([sample],
+                                             return_distance=False)[0][1:]
+                knn[index].update(list(r_neighs))
+                for neigh in r_neighs:
+                    nb[neigh] += 1
+                    rnn[neigh].add(index)
+
+            cnt[r] = np.count_nonzero((np.array(list(nb.values())) == 0))
+            if r > 2 and cnt[r] == cnt[r - 1]:
+                r -= 1
+                break
+            r += 1
+
+        for index in range(self.n_id):
+            nan[index] = knn[index].intersection(rnn[index])
+
+        return nan, r
+
+    def __enane(self, fx, nan, r):
+        es = []
+        es_pred = []
+        local_structure = self.structure_stdnpf.copy(deep=True)
+        base_estimator = KNeighborsClassifier(n_neighbors=r,
+                                              metric=self.distance_metric)
+
+        labeled_data = local_structure.loc[local_structure[
+                                               'label'] != -1]
+        nan_unlabeled = local_structure.loc[fx]
+        data = pd.concat([labeled_data, nan_unlabeled], join='inner')
+
+        enane_model = SelfTrainingClassifier(base_estimator)
+        enane_model.fit(data['sample'].tolist(), data['label'].tolist())
+
+        enane_pred = enane_model.predict(nan_unlabeled['sample'].tolist())
+
+        for (row_index, _), pred in zip(nan_unlabeled.iterrows(),
+                                        enane_pred):
+            usefulness = 0
+            harmfulness = 0
+            for neigh in nan[row_index]:
+                if local_structure.loc[neigh, 'label'] == pred:
+                    usefulness += 1
+                else:
+                    harmfulness += 1
+
+            if usefulness >= harmfulness:
+                es.append(row_index)
+                es_pred.append(pred)
+
+        return es, es_pred
+
+    def __init_values(self, l, u, y):
+        self.y = y
+        self.l = l
+        self.u = u
         self.data = np.concatenate((l, u), axis=0)
         self.n_id = self.data.shape[0]
-        self.distances, self.max_dis, self.min_dis = self.build_distance()
-        self.dc = self.select_dc()
-        self.rho = self.local_density()
-        self.delta, self.nneigh = self.min_neighbor_and_distance()
+        self.distances, self.max_dis, self.min_dis = self.__build_distance()
+        self.dc = self.__select_dc()
+        self.rho = self.__local_density()
+        self.delta, self.nneigh = self.__min_neighbor_and_distance()
+        self.__structure()
+
+    def _fit_without(self):
         if self.classifier is None:
             self.classifier = SVC()
-
-        # Step 1
-        self.__structure()
+        count = 1
+        self.order = dict.fromkeys(range(self.n_id), 0)
 
         # Step 2
         while True:
@@ -186,6 +264,7 @@ class SemiSupervisedSelfTraining:
             for next_row in next_rows:
                 if next_row not in samples_labeled_index:
                     next_unlabeled.append(next_row)
+                    self.order[next_row] = count
             if len(next_unlabeled) == 0:
                 break
             unlabeled_next_of_labeled = self.structure.loc[next_unlabeled]
@@ -196,6 +275,9 @@ class SemiSupervisedSelfTraining:
             # 2.c
             for new_label, pos in zip(y_pred, next_unlabeled):
                 self.structure.at[pos, 'label'] = new_label
+
+            # For STDPNF
+            count += 1
 
         # Step 3
         while True:
@@ -210,6 +292,7 @@ class SemiSupervisedSelfTraining:
                 if prev_row not in samples_labeled_index and prev_row is not \
                         None:
                     prev_unlabeled.append(prev_row)
+                    self.order[prev_row] = count
             if len(prev_unlabeled) == 0:
                 break
             unlabeled_prev_of_labeled = self.structure.loc[prev_unlabeled]
@@ -220,6 +303,50 @@ class SemiSupervisedSelfTraining:
             # 3.c
             for new_label, pos in zip(y_pred, prev_unlabeled):
                 self.structure.at[pos, 'label'] = new_label
+
+            # For STDPNF
+            count += 1
+
+    def _fit_stdpnf(self):
+        """
+        Self Training based on Density Peaks and a parameter-free noise
+        filter.
+        """
+
+        self.__discover_structure()
+
+        nan, lambda_param = self.__nan_search()
+        self.classifier_stdpnf = KNeighborsClassifier(
+            n_neighbors=self.k, metric=self.distance_metric)
+        self.classifier_stdpnf.fit(self.l, self.y)
+        count = 1
+
+        while count <= max(self.order.values()):
+            unlabeled_rows = self.structure_stdnpf.loc[self.structure_stdnpf[
+                                                           'label'] == -1].\
+                index.to_list()
+            unlabeled_indexes = []
+            for row in unlabeled_rows:
+                if self.order[row] == count:
+                    unlabeled_indexes.append(row)
+
+            filtered_indexes, filtered_labels = self.__enane(
+                unlabeled_indexes, nan, lambda_param)
+
+            self.structure_stdnpf.at[filtered_indexes, 'label'] = \
+                filtered_labels
+
+            labeled_data = self.structure_stdnpf.loc[self.structure_stdnpf[
+                                                         'label'] != -1]
+            self.classifier_stdpnf.fit(
+                labeled_data['sample'].tolist(), labeled_data['label'].tolist())
+
+            count += 1
+
+        labeled_data = self.structure_stdnpf.loc[self.structure_stdnpf[
+                                                     'label'] != -1]
+        self.classifier_stdpnf.fit(
+            labeled_data['sample'].tolist(), labeled_data['label'].tolist())
 
     def fit(self, l, u, y):
         """Fit method."""
@@ -232,9 +359,11 @@ class SemiSupervisedSelfTraining:
         le = LabelEncoder()
         le.fit(y)
         y = le.transform(y)
-        self.y = y
-        if self.filtering is False:
-            self._fit_without(l, u)
+        self.__init_values(l, u, y)
+        if self.filtering:
+            self._fit_stdpnf()
+        else:
+            self._fit_without()
 
     def predict(self, src):
         return self.classifier.predict(src)
