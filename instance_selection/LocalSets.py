9b5151aacfbf1b8d6ce144659057a1a9a7241c90
Daniel Puente <dpr1005@alu.ubu.es>
2022-03-18 13:35:00 +0100

    Implemented LSBo and added inheritance #130 #131

--- instance_selection/LocalSets.py
+++ instance_selection/LocalSets.py
@@ -3,34 +3,34 @@
 # @Filename:    LocalSets.py
 # @Author:      Daniel Puente Ramírez
 # @Time:        18/3/22 11:14
+# @Version:     2.0
 import sys
 
-import numpy as np
 import pandas as pd
-from sklearn.datasets import load_iris
 from sklearn.metrics import pairwise_distances
 
 
-class LSSm:
+class LocalSets:
     def __init__(self):
-        self.__local_sets = None
+        self.local_sets = None
         self.n_id = 0
 
-    def __compute_local_sets(self, instances, labels):
+    def compute_local_sets(self, instances, labels):
+        self.n_id = len(instances)
         structure = dict.fromkeys(range(self.n_id))
         distances = pairwise_distances(instances)
 
         for index, (sample, label) in enumerate(zip(instances, labels)):
             closest_enemy_distance = sys.maxsize
             closest_enemy_sample = None
-            for index2, (sample2, label2) in enumerate(zip(instances, labels)):
+            for index2, (_, label2) in enumerate(zip(instances, labels)):
                 if index == index2 or label == label2:
                     continue
                 if distances[index][index2] < closest_enemy_distance:
                     closest_enemy_distance = distances[index][index2]
                     closest_enemy_sample = index2
             structure[index] = [sample, [], None, closest_enemy_distance,
-                                closest_enemy_sample]
+                                closest_enemy_sample, label]
 
         for index, (sample, label) in enumerate(zip(instances, labels)):
             neighs = []
@@ -42,15 +42,27 @@ class LSSm:
             structure[index][1] = neighs
             structure[index][2] = len(neighs)
 
-        self.__local_sets = pd.DataFrame(structure, index=['sample',
-                                                           'index_ls',
-                                                           'LSC', 'LSR',
-                                                           'enemy']) \
+        self.local_sets = pd.DataFrame(structure, index=['sample',
+                                                         'index_ls',
+                                                         'LSC', 'LSR',
+                                                         'enemy', 'label']) \
             .transpose()
 
-    def __usefulness(self, e):
-        local_sets = self.__local_sets['index_ls'].values
+    def sort_asc_lsc(self):
+        self.local_sets = self.local_sets.sort_values(by='LSC')
+
+    def usefulness(self, e):
+        local_sets = self.local_sets
+        local_sets = local_sets['index_ls'].values
         return len([x for x in local_sets if e in x])
+
+    def get_local_sets(self):
+        return self.local_sets
+
+
+class LSSm(LocalSets):
+    def __init__(self):
+        super().__init__()
 
     def filter(self, instances, labels):
         names = instances.keys()
@@ -64,28 +76,53 @@ class LSSm:
         s_samples = []
         s_labels = []
 
-        self.__compute_local_sets(instances, labels)
+        super(LSSm, self).compute_local_sets(instances, labels)
         for index in range(self.n_id):
-            usefulness = self.__usefulness(index)
+            usefulness = super(LSSm, self).usefulness(index)
             try:
-                harmfulness = self.__local_sets['enemy'].value_counts()[index]
+                harmfulness = super(LSSm, self).get_local_sets()['enemy']
+                harmfulness = harmfulness.value_counts()[index]
             except KeyError:
                 harmfulness = 0
             if usefulness >= harmfulness:
                 s_samples.append(instances[index])
                 s_labels.append(labels[index])
 
-        X = pd.DataFrame(s_samples, columns=names)
+        x = pd.DataFrame(s_samples, columns=names)
         y = pd.DataFrame(s_labels)
-        return X, y
+        return x, y
 
 
-if __name__ == '__main__':
-    iris = load_iris()
-    print(iris.keys())
-    X1 = pd.DataFrame(iris.data, columns=iris.feature_names)
-    y1 = iris.target
+class LSBo(LocalSets):
+    def __init__(self):
+        super(LSBo, self).__init__()
 
-    model = LSSm()
-    X2, y2 = model.filter(X1, y1)
-    print(len(X2))
+    def filter(self, instances, labels):
+        names = instances.keys()
+        if len(instances) != len(labels):
+            raise ValueError(
+                f'The dimension of the labeled data must be the same as the '
+                f'number of labels given. {len(instances)} != {len(labels)}'
+            )
+        self.n_id = len(instances)
+        lssm = LSSm()
+        instances, labels = lssm.filter(instances, labels)
+        instances = instances.to_numpy()
+        labels = labels.to_numpy().flatten()
+        super(LSBo, self).compute_local_sets(instances, labels)
+        super(LSBo, self).sort_asc_lsc()
+
+        s_indexes = []
+        for index, row in super(LSBo, self).get_local_sets().iterrows():
+            is_in = False
+            for ls_index in row['index_ls']:
+                if ls_index in s_indexes:
+                    is_in = True
+                    break
+            if not is_in:
+                s_indexes.append(index)
+
+        x = pd.DataFrame(instances[s_indexes], columns=names)
+        y = pd.DataFrame(labels[s_indexes])
+
+        return x, y
